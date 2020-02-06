92a274772ccd22a8221070e1fedd53b036855ad2
bumjin0116 <60742598+bumjin0116@users.noreply.github.com>
2020-02-06 22:55:41 +0900

    Bumjin
    근호 소스코드 업데이트

--- Test.js
+++ Test.js
@@ -1,50 +1,104 @@
 const puppeteer = require('puppeteer');
 const excel = require('xlsx');
+const glob = require( 'glob' ) ;
 
-async function crawlProduct(url) {
+let myArgs = process.argv.slice(2).map(Number)
+,   start = myArgs[0] || 0
+,   end = myArgs[1] || 10
+,   product_URL = null 
+,   dataObj = null 
+;
+
+async function crawlProduct(url, num) {
+
+    let strTitle 
+    ,   strStock
+    ,   strPrice = null 
+    ;
+
+    console.log({ num, url }) ;
+
+    /* 아마존 사이트가 아닐 경우 */
+    if( url.indexOf( 'amazon.com' ) < 0 ){
+        console.log( '아마존 사이트가 아닙니다' ) ;
+        return ;
+    }
+
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(url);
 
-    var [return_val];
-
+    /* 제목 */
     const [el] = await page.$x('//*[@id="productTitle"]');      // Product Title 
     const title = await el.getProperty('textContent');
     const jsonTitle = await title.jsonValue();
-    const strTitle= jsonTitle.trim();
+    strTitle= jsonTitle.trim();
+    console.log({strTitle});
 
+    /* 재고 */
     const [el2] = await page.$x('//*[@id="availability"]/span');        // Stock availability
     const stock = await el2.getProperty('textContent');
     const jsonStock = await stock.jsonValue();
-    const strStock= jsonStock.trim();
-
+    strStock= jsonStock.trim();
+    console.log({strStock});   
+    
+    /* 가격 */
     const [el3] = await page.$x('//*[@id="price_inside_buybox"]');      // Price 
-    const price = await el3.getProperty('textContent');
-    const jsonPrice = await price.jsonValue();
-    const strPrice = jsonPrice.trim().replace("US$","");
 
-    console.log({strTitle,strStock,strPrice});
+    /* el3을 찾아서 존재한다면 품절이 아니다. */
+    if( el3 ) {
+        const price = await el3.getProperty('textContent');
+        const jsonPrice = await price.jsonValue();
+        strPrice = jsonPrice.trim().replace("US$","");    
+    } else {    /* 품절 일 경우 0 처리 */
+        strPrice = 0 ;
+    }
 
+    console.log({strPrice});   
+    // console.log({ num, url, strTitle, strStock, strPrice }) ;
     browser.close();
+    return { num, url, strTitle, strStock, strPrice } ;
 }
 
+
+function findFiles( url, num ){
+    return new Promise((resolve, reject) => {
+        let result = crawlProduct( url, num ) ;
+        if( result ) {
+            resolve( result ) ;
+        }
+    })
+}
+
+
+
+
 function excelRead(fileName, i) {
-    var workbook = excel.readFile(fileName);
-    var worksheet = workbook.Sheets['미국'];
-    var address_hyperlink_col = 'G';
-    var address_hyperlink_low = 4+i;
-    var combined_address = address_hyperlink_col+String(address_hyperlink_low);
+    
+    let workbook = excel.readFile(fileName)
+    ,   worksheet = workbook.Sheets['미국']
+    ,   address_hyperlink_col = 'G'
+    ,   address_hyperlink_low = 4+i
+    ,   combined_address = address_hyperlink_col+String(address_hyperlink_low)
+    ,   desired_URL = worksheet[combined_address]
+    ,   strURL
+    ;   
 
-    var desired_URL = worksheet[combined_address];
-    var strURL = desired_URL.f.replace("\"","").replace("HYPERLINK(","").replace("\",\"A\")", "");
+    // console.log({ desired_URL }) ;
 
-    console.log({strURL});
-    //crawlProduct(strURL);
+    /* 링크 데이터 값의 형태가 다른 경우 오류가 나기 때문에 null 값으로 넘긴다. */
+    strURL = !desired_URL.f ? null : strURL = desired_URL.f.replace("\"","").replace("HYPERLINK(","").replace("\",\"A\")", ""); 
+    
+    /* if( !desired_URL.f ) {
+        strURL = null ;
+    } else {
+        strURL = desired_URL.f.replace("\"","").replace("HYPERLINK(","").replace("\",\"A\")", ""); 
+    } */
 
     return strURL;
 }
 
-/*function generateAlphabetLow(i) {
+function generateAlphabetLow(i) {
     if (i>26) {
         firstAlpha = i/27;
         secondAlpha = i-firstAlpha*26;
@@ -58,11 +112,24 @@ function excelRead(fileName, i) {
         console.log({combindedAlpha});
     }
     //return combindedAlpha;
-}*/
-
-generateAlphabetLow(27);
-
-for(i=0;i<100;i++) {
-    var product_URL = excelRead('상품등록표.xlsx', i);
-    crawlProduct(product_URL);
 }
+
+// generateAlphabetLow(27);
+
+/* for(i=0;i<len;i++) {
+    product_URL = excelRead('상품등록표.xlsx', i);
+    crawlProduct(product_URL);
+    console.log({ product_URL }) ;
+} */
+
+(async () => {
+    for(i=start;i<end;i++) {
+        // console.log( '\n', i, ' 번째 시작!' ) ;
+        product_URL = excelRead('상품등록표_2.xlsx', i);
+        if( product_URL == null ) {            
+            console.log( `${i}번째 상품의 링크값에 오류가 있습니다.` ) ;
+        } else {
+            dataObj = await findFiles( product_URL, i ) ;
+        }
+    }
+})();
